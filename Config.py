30fd5d5ba8136ff877b420a909d6e0cdaeb1abe3
Oresti Theodoridis <orestitheodoridis@Orestis-MBP.lan>
2020-07-17 07:43:28 +0200

    just a commit

--- Config.py
+++ Config.py
@@ -8,10 +8,8 @@ def f(thetas, t, eta, kappa, I):
 
 def I_fun(an, thetas, n):
     S = 0
-    #print("Thetas = ", thetas)
     for i in range(len(thetas)):
         s = (1-np.cos(thetas[i]))**n
-        #print("s = ", s)
         S += s
         
     I = an/len(thetas) * S
@@ -29,16 +27,12 @@ def factorial(n):
 def euler(f, eta, kappa, y0=1, h=1e-3, N=100):
     y = np.ones((len(y0), N))
     y[:, 0] = y0
-    #print("y first =", y)
     t = 0
     n = 2
     for i in range(N-1):
         an = an_fun(n=n)
-        #print("an = ", an)
         I = I_fun(an, y[:, i], n=n)
-        #print("I = ", I)
         y[:, i+1] = y[:, i] + h*f(thetas=y[:, i], t=t, eta=eta, kappa=kappa, I=I)
-        #print("y = ", y)
         t += h
     return(y)
 
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from Config import *
 
 
-def get_chaos():
+def get_chaos(plotting=True):
     M = 30000
     he = 1e-2
 
     time = np.linspace(0, M, M)/100
 
 
-    thetas1 = np.array([0, 1, 6, 9, 2])
-    thetas2 = np.array([1e-4, 1, 6, 9, 2])
+    thetas1 = np.array([0, 1, 6])
+    thetas2 = np.array([1e-4, 1, 6])
 
     A = np.sin(euler(f,  eta=0.2, kappa=-0.75, h=he, y0=thetas1, N=M))[0]
     B = np.sin(euler(f, eta=0.2, kappa=-0.75, h=he, y0=thetas2, N=M))[0]
